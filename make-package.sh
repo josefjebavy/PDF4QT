6fff8e3a7ac63176446398ef2a50afc4cc309bf2
josefjebavy <email@josefjebavy.cz>
2023-07-27 10:11:58 +0200

    move Plugin.so to subdir,  add dependences: qt6-speech-speechd-plugin, qt6-speech-flite-plugin

--- make-package.sh
+++ make-package.sh
@@ -4,6 +4,13 @@ name="PDF4QT"
 
 
 cd bin_release/
+
+mkdir -p install/lib/pdf4qt
+mv -f install/lib/*Plugin.so*  install/lib/pdf4qt/
+
+#mkdir -p install/usr/bin/
+#mv -f install/bin/* install/usr/bin/
+
 mkdir -p install/DEBIAN/
 file="install/DEBIAN/control"
 
@@ -13,7 +20,7 @@ echo "Version: ${version}" >>$file
 echo "Section: base" >>$file
 echo "Priority: optional" >>$file
 echo "Architecture: amd64" >>$file
-echo "Depends: libqt6gui6, libqt6xml6, libqt6svg6, libqt6openglwidgets6, libtbb12,  libqt6printsupport6, libqt6texttospeech6">>$file
+echo "Depends: libqt6gui6, libqt6xml6, libqt6svg6, libqt6openglwidgets6, libtbb12,  libqt6printsupport6, libqt6texttospeech6, qt6-speech-speechd-plugin, qt6-speech-flite-plugin">>$file
 echo "Maintainer: Jakub Melka" >>$file
 echo "Description: This software is consisting of PDF rendering library, and several applications, such as advanced document viewer, command line tool, and document page manipulator application. Software is implementing PDF functionality based on PDF Reference 2.0. It is written and maintained by Jakub Melka." >>$file
 
